93925afe2338dc7b211b5d95e1baf8b8f57917b2
dotxdaemon <129806199+dotxdaemon@users.noreply.github.com>
2025-09-19 15:33:45 -0700

    Add greeting blog post

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -1,1 +1,8 @@
-window.BLOG_POSTS = [];
+window.BLOG_POSTS = [
+  {
+    title: "Simple Greeting",
+    date: "2024-06-02",
+    excerpt: "A quick hello to everyone reading.",
+    body: "hi everybody",
+  },
+];
